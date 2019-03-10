ce5054e267e324175fa0e4544bf3bee37572ca3e
XTF1182741213 <1182741213@qq.com>
2019-03-11 01:13:39 +0800

    test push

--- app/src/main/java/cn/tengfeistudio/forum/api/ApiFactory.java
+++ app/src/main/java/cn/tengfeistudio/forum/api/ApiFactory.java
@@ -4,6 +4,7 @@ package cn.tengfeistudio.forum.api;
 import cn.tengfeistudio.forum.api.api.PlusClubApi;
 import cn.tengfeistudio.forum.api.api.WeatherApi;
 
+
 public class ApiFactory {
     protected static final Object monitor = new Object();
     static PlusClubApi plusClubApi = null;
